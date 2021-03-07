26c8f006da4ccf5cef4f44c16806caf04fcc8af9
Andrei Ionut Hrisca <andr3y_30@yahoo.com>
2021-03-07 12:43:46 +0100

    Index semantico

---
+++ indexPrueba.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+<meta charset='UTF-8'>
+<title>Andrei Portfolio</title>
+<meta name='description' content='Portfolio para la asignatura Diseño de DAW.'>
+<meta name='keywords' content='PHP, HTML, SEO, SCSS'>
+<meta name='author' content='Andrei Ionut Hrisca'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<link rel='shortcut icon' href='#' sizes='16x16' type='image/ico'>
+<link rel='stylesheet' href='estilos.css'>
+</head>
+<body>
+
+</body>
+</html>
