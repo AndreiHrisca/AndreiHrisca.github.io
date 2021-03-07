0a660781aa2dbd0b8b8b09954035ba673bdfd2c2
Andrei Ionut Hrisca <andr3y_30@yahoo.com>
2021-03-08 00:18:20 +0100

    Index nuevo

--- indexPrueba.php
+++ indexPrueba.php
@@ -1,16 +1,127 @@
 <!DOCTYPE html>
 <html lang='es'>
 <head>
-<meta charset='UTF-8'>
-<title>Andrei Portfolio</title>
-<meta name='description' content='Portfolio para la asignatura Diseño de DAW.'>
-<meta name='keywords' content='PHP, HTML, SEO, SCSS'>
-<meta name='author' content='Andrei Ionut Hrisca'>
-<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-<link rel='shortcut icon' href='#' sizes='16x16' type='image/ico'>
-<link rel='stylesheet' href='estilos.css'>
+    <meta charset='UTF-8'>
+    <meta name='author' content='Andrei Ionut Hrisca'>
+    <title>Andrei Portfolio</title>
+    <meta name="title" content="Andrei Portfolio">
+    <meta name='description' content='Portfolio para la asignatura Diseño de DAW.'>
+    <meta name='keywords' content='PHP, HTML, SEO, SCSS'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <!-- Favicon -->
+    <link rel='shortcut icon' href='./assets/img/favicon.ico' sizes='16x16' type='image/ico'>
+    <!-- Estilos -->
+    <link rel='stylesheet' href='./css/indexExamen.css'>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!-- Scripts -->
+
+
 </head>
 <body>
+    <main>
+        <span id="inicio"></span>
+        <header>
+            <h1>Sitio web Andrei</h1>
+            <p>Esto es un portfolio realizado por Andrei Ionut Hrisca, en la asignatura de Diseño de interfaces web, en el año 2021, curso Desarrollo de aplicaciones web.</p>
+        </header>
 
+        <section>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/wordpress.jpg" alt="Perfil picture">
+                    <h3>WordPress</h3>
+                    <figcaption>Sitio web creado con WordPress.</figcaption>
+                    <a href="#" title="Wordpress">Visitar sitio</a>
+                </picture>
+                
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/media.jpg" alt="Perfil picture">
+                    <h3>Promoción DAW 2021</h3>
+                    <figcaption>Media queries y responsive.</figcaption>
+                    <a href="./Segundo_Trimestre/classmates.html" title="Promoción DAW 2021">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/flex.jpg" alt="Imagenes con flexbox">
+                    <h3>Flexbox</h3>
+                    <figcaption>Flexbox y semantica.</figcaption>
+                    <a href="./Segundo_Trimestre/felxbox.html" title="Flexbox">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/login.jpg" alt="Login y registro">
+                    <h3>Sesión</h3>
+                    <figcaption>Probando Sesión en PHP.</figcaption>
+                    <a href="login.php" title="Sesión">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/contacto.jpg" alt="Formulario de contacto">
+                    <h3>Contacto</h3>
+                    <figcaption>Formulario de contacto.</figcaption>
+                    <a href="./Segundo_Trimestre/formulario/datos-personales-enviophp.html" title="Contacto">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/benchmark.jpg" alt="Benchmark test">
+                    <h3>Benchmark</h3>
+                    <figcaption>Probando el servidor con nuestro Benchmark.</figcaption>
+                    <a href="./Segundo_Trimestre/benchmark.php" title="Benchmark">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/estilos.jpg" alt="Perfil picture">
+                    <h3>Guía de estilos</h3>
+                    <figcaption>Descripción.</figcaption>
+                    <a href="./Segundo_Trimestre/guiaDisenoExamen.html" title="Guía de estilos">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/pelicula.jpg" alt="Pelicula recomendada">
+                    <h3>Pelicula</h3>
+                    <figcaption>Pelicula con contenido multimedia.</figcaption>
+                    <a href="#" title="Pelicula">Visitar sitio</a>
+                </picture>
+            </article>
+
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/vintage.jpg" alt="HTML y CSS">
+                    <h3>Vintage</h3>
+                    <figcaption>HTML desde 0 y estilos con CSS.</figcaption>
+                    <a href="#" title="Vintage">Visitar sitio</a>
+                </picture>
+            </article>
+        </section>
+
+        <footer>
+            <small>Derechos de autor reservados.</small>
+            <address>
+                <span>Escrito por <a href="./Segundo_Trimestre/formulario/datos-personales-enviophp.html">Andrei Ionut Hrisca</a></span>
+                <span>Visitame en:</span>
+                <span>linkedin.com/in/andreiionuthrisca</span>
+                <span>Calle Ejemplo, Madrid</span>
+                <span>España</span>
+            </address>
+            <span><a href="#inicio" title="Inicio de página">Subir</a></span>
+        </footer>
+    </main>
 </body>
 </html>
