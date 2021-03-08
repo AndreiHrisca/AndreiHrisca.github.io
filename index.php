56f3db38981e95df3efb18af5d2bc8523cc670a2
Andrei Ionut Hrisca <andr3y_30@yahoo.com>
2021-03-08 10:27:17 +0100

    Examen terminado

--- index.php
+++ index.php
@@ -1,133 +1,134 @@
-<!--<?php
-
-
-
-// Initialize the session
-
-//session_start();
-
- 
-
-// Check if the user is logged in, if not then redirect him to login page
-
-//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-
-//    header("location: login.php");
-
-//    exit;
-
-//}
-
-?> -->
-
-
-
 <!DOCTYPE html>
-
-<html lang="es">
-
+<html lang='es'>
 <head>
-
-    <meta charset="UTF-8">
-
-    <title>Ejercicios Diseño de interfaces</title>
-
-    <meta name="description" content="Ejercicios diseño de interfaces 2 DAW.">
-
-    <meta name="keywords" content="HTML, css, scss, php">
-
-    <meta name="author" content="Andrei Ionut Hrisca">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="shortcut icon" href="./assets/img/favicon.ico" sizes="16x16" type="image/ico">
-
-    <link rel="stylesheet" href="./css/indexEstilos.css">
-
+    <meta charset='UTF-8'>
+    <meta name='author' content='Andrei Ionut Hrisca'>
+    <title>Andrei Portfolio</title>
+    <meta name="title" content="Andrei Portfolio">
+    <meta name='description' content='Portfolio para la asignatura Diseño de DAW.'>
+    <meta name='keywords' content='PHP, HTML, SEO, SCSS'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <!-- Favicon -->
+    <link rel='shortcut icon' href='./assets/img/favicon.ico' sizes='16x16' type='image/ico'>
+    <!-- Estilos -->
+    <link rel='stylesheet' href='./css/indexExamen.css'>
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
-
-
-
+    <!-- Scripts -->
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
+    <main>
+        <span id="inicio"></span>
+        <header>
+            <h1>Andrei Ionut Hrisca</h1>
+            <p>Esto es un portfolio realizado por Andrei Ionut Hrisca, en la asignatura de Diseño de interfaces web, en el año 2021, curso Desarrollo de aplicaciones web.</p>
+            <nav>
+                <span>Desarrollo de aplicaciones web</span>
+                <button class="switch" id="switch">
+                    <span><i class="fas fa-sun"></i></span>
+                    <span><i class="fas fa-moon"></i></span>
+                </button>
+            </nav>
+        </header>
 
-    <!--<div id="DatosUsuario">
+        <section>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/wordpress.jpg" alt="Perfil picture">
+                    <h3>WordPress</h3>
+                    <figcaption>Sitio web creado con WordPress.</figcaption>
+                    <a href="./wordpress" title="Wordpress">Visitar sitio</a>
+                </picture>
+                
+            </article>
 
-        <h3>Hola, <b><?php //echo htmlspecialchars($_SESSION["username"]); ?></b></h3>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/media.jpg" alt="Perfil picture">
+                    <h3>RWD</h3>
+                    <figcaption>Rejilla de resultado de los compañeros. (Media queries y responsive)</figcaption>
+                    <a href="./Segundo_Trimestre/classmates.html" title="Promoción DAW 2021">Visitar sitio</a>
+                </picture>
+            </article>
 
-        <a href="logout.php" class="btn btn-danger">Cierra la sesión</a>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/flex.jpg" alt="Imagenes con flexbox">
+                    <h3>Flexbox</h3>
+                    <figcaption>Flexbox y semantica.</figcaption>
+                    <a href="./Segundo_Trimestre/felxbox.html" title="Flexbox">Visitar sitio</a>
+                </picture>
+            </article>
 
-    </div>-->
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/contacto.jpg" alt="Formulario de contacto">
+                    <h3>Contacto</h3>
+                    <figcaption>Formulario de contacto.</figcaption>
+                    <a href="./Segundo_Trimestre/formulario/datos-personales-enviophp.html" title="Contacto">Visitar sitio</a>
+                </picture>
+            </article>
 
-    
-    <!-- Lista de ejercicios -->
-    <div class="contenedor">
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/benchmark.jpg" alt="Benchmark test">
+                    <h3>Benchmark</h3>
+                    <figcaption>Probando el servidor con nuestro Benchmark.</figcaption>
+                    <a href="./Segundo_Trimestre/benchmark.php" title="Benchmark">Visitar sitio</a>
+                </picture>
+            </article>
 
-      <h2>Andrei Website</h2>
-      <a class="hoverable" href="./wordpress/index.php">WordPress</a>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/estilos.jpg" alt="Perfil picture">
+                    <h3>Guía de estilos</h3>
+                    <figcaption>Descripción.</figcaption>
+                    <a href="./Segundo_Trimestre/guiaDisenoExamen.html" title="Guía de estilos">Visitar sitio</a>
+                </picture>
+            </article>
 
-      <a class="hoverable" href="./Segundo_Trimestre/classmates.html">Media Queries</a>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/pelicula.jpg" alt="Pelicula recomendada">
+                    <h3>Peliculón</h3>
+                    <figcaption>Pelicula con contenido multimedia.</figcaption>
+                    <a href="./Segundo_Trimestre/Pelicula_con_scss.html" title="Pelicula">Visitar sitio</a>
+                </picture>
+            </article>
 
-      <a class="hoverable" href="./Segundo_Trimestre/felxbox.html">Flex & Semantica</a>
+            <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/vintage.jpg" alt="HTML y CSS">
+                    <h3>Vintage</h3>
+                    <figcaption>HTML desde 0 y estilos con CSS.</figcaption>
+                    <a href="./Primer_Trimestre/Ej1_texto.html" title="Vintage">Visitar sitio</a>
+                </picture>
+            </article>
 
-      <a class="hoverable" href="./registro.php">Probando sesión</a>
+            
+          <!--  <article>
+                <picture>
+                    <img src="./assets/img/IndexPages/login.jpg" alt="Login y registro">
+                    <h3>Sesión</h3>
+                    <figcaption>Probando Sesión en PHP.</figcaption>
+                    <a href="login.php" title="Sesión">Visitar sitio</a>
+                </picture>
+            </article> -->
+        </section>
 
-      <a class="hoverable" href="./Segundo_Trimestre/formulario/datos-personales-enviophp.html" target="_blank">Contacto</a>
-
-      <a class="hoverable" href="./Segundo_Trimestre/benchmark.php">Benchmark</a>
-
-      <a class="hoverable" href="./Segundo_Trimestre/guiaDisenoExamen.html">Guía de estilos</a>
-
-      <a class="hoverable" href="./Segundo_Trimestre/Pelicula_con_scss.html">Peliculón</a>
-
-      <a class="hoverable" href="./Primer_Trimestre/Ej1_texto.html">Vintage</a>
-
-      
-    </div>
-
-
-
-
-
-    <!-- Cursor -->
-
-    <div class="cursor">
-
-        <div class="circulo circulo_grande">
-
-          <svg height="30" width="30">
-
-            <circle cx="15" cy="15" r="12" stroke-width="0"></circle>
-
-          </svg>
-
-        </div>
-
-        
-
-        <div class="circulo circulo_pequeno">
-
-          <svg height="10" width="10">
-
-            <circle cx="5" cy="5" r="4" stroke-width="0"></circle>
-
-          </svg>
-
-        </div>
-
-    </div>
-
-
-
-    <script src="./js/main.js"></script>
-
-
-
+        <footer>
+            <small>Derechos de autor reservados.</small>
+            <address>
+                <span>Escrito por <a href="./Segundo_Trimestre/formulario/datos-personales-enviophp.html">Andrei Ionut Hrisca</a></span>
+                <span>Visitame en:</span>
+                <span>linkedin.com/in/andreiionuthrisca</span>
+            </address>
+            <span><a href="#inicio" title="Inicio de página">Subir</a></span>
+        </footer>
+    </main>
+    <!-- Scripts -->
+    <script src="./js/themes.js"></script>
 </body>
-
 </html>
